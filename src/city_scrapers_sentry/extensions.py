ed12a0b61f49cfff392ec297489381a00576e639
Daniel Simmons-Ritchie <37225902+SimmonsRitchie@users.noreply.github.com>
2024-01-12 12:01:11 -0600

    Replace raven with sentry_sdk

--- src/city_scrapers_sentry/extensions.py
+++ src/city_scrapers_sentry/extensions.py
@@ -15,18 +15,18 @@ from .utils import get_client, get_release, response_to_dict
 
 class Errors(object):
     def __init__(self, dsn=None, client=None, **kwargs):
-        self.client = client if client else get_client(dsn, **kwargs)
+        self.client = get_client(dsn, **kwargs)  # Initialize Sentry SDK
 
     @classmethod
-    def from_crawler(cls, crawler, client=None, dsn=None):
+    def from_crawler(cls, crawler, dsn=None):
         release = crawler.settings.get("RELEASE", get_release(crawler))
 
         dsn = os.environ.get("SENTRY_DSN", crawler.settings.get("SENTRY_DSN", None))
         if dsn is None:
             raise NotConfigured("No SENTRY_DSN configured")
-        o = cls(dsn=dsn, release=release)
-        crawler.signals.connect(o.spider_error, signal=signals.spider_error)
-        return o
+        extension = cls(dsn=dsn, release=release)
+        crawler.signals.connect(extension.spider_error, signal=signals.spider_error)
+        return extension
 
     def spider_error(
         self, failure, response, spider, signal=None, sender=None, *args, **kwargs
@@ -38,18 +38,17 @@ class Errors(object):
 
         extra = {
             "sender": sender,
-            "spider": spider.name,
             "signal": signal,
             "failure": failure,
             "response": res_dict,
             "traceback": "\n".join(traceback.getvalue().split("\n")[-5:]),
         }
-        msg = self.client.captureMessage(
-            message="[{}] {}".format(spider.name, repr(failure.value)), extra=extra
-        )
+        
+        with self.client.push_scope() as scope:
+            for key, value in extra.items():
+                scope.set_tag('spider', spider.name)
+                scope.set_extra(key, value)
+            self.client.capture_exception(failure.value)
 
-        ident = self.client.get_ident(msg)
+        logging.log(logging.WARNING, "Sentry Exception captured")
 
-        logging.log(logging.WARNING, "Sentry Exception ID '%s'" % ident)
-
-        return ident
--- src/city_scrapers_sentry/utils.py
+++ src/city_scrapers_sentry/utils.py
@@ -1,58 +1,35 @@
 import os
 import time
-import logging
-
 import inspect
 import pkg_resources
-
-from twisted.python import log
 
 from scrapy.utils.project import get_project_settings
 from scrapy.http import Headers  # noqa
 from scrapy.responsetypes import responsetypes
 
-from raven import Client
-from raven.conf import setup_logging
-from raven.handlers.logging import SentryHandler
-from raven.transport.requests import RequestsHTTPTransport
-
-SENTRY_DSN = os.environ.get("SENTRY_DSN", None)
+import sentry_sdk  # New import for Sentry SDK
 
 settings = get_project_settings()
 
+SENTRY_DSN = os.environ.get("SENTRY_DSN", None)
+
 
 def get_client(dsn=None, **options):
-    """gets a scrapy client"""
-    return Client(
-        dsn or settings.get("SENTRY_DSN", SENTRY_DSN),
-        transport=RequestsHTTPTransport,
-        **options,
-    )
+    """Initializes Sentry SDK client."""
+    sentry_dsn = dsn or settings.get("SENTRY_DSN", SENTRY_DSN)
+    if sentry_dsn:
+        sentry_sdk.init(dsn=sentry_dsn, **options)
+    return sentry_sdk
 
 
 def get_release(crawler):
-    """gets the release from a given crawler"""
+    """Gets the release from a given crawler."""
     try:
         mod = inspect.getmodule(crawler.spidercls)
         pkg = mod.__package__.replace(".", "-")
         return pkg_resources.get_distribution(pkg).version
     except Exception:
         return None
-
-
-def init(dsn=None):
-    """Redirect Scrapy log messages to standard Python logger"""
-
-    observer = log.PythonLoggingObserver()
-    observer.start()
-
-    dict_config = settings.get("LOGGING")
-    if dict_config is not None:
-        assert isinstance(dict_config, dict)
-        logging.dictConfig(dict_config)
-
-    handler = SentryHandler(dsn)
-    setup_logging(handler)
 
 
 def response_to_dict(response, spider, include_request=True, **kwargs):
